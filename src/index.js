6dbed21902125fb063e44cb2cf5c5803823f3506
save14 <save999999@gmail.com>
2017-09-23 16:10:37 +0300

    Add main code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 /* @flow */
+
+const requestp: Function = require('request-promise');
+
+class SmsActivate {
+  static DEBUG = false;
+  static defaults = {
+    url: 'http://sms-activate.ru/stubs/handler_api.php'
+  }
+
+  _apikey: string;
+  _url: string = SmsActivate.defaults.url;
+
+  constructor(apikey: string, url?: string) {
+    if (typeof apikey !== 'string')
+      throw new TypeError('API Key must be a string');
+
+    this._apikey = apikey;
+    if (url) this._url = url;
+  }
+
+  async _method(method: string, opts?: Object): Promise<string> {
+    const req = Object.assign(opts || {}, {
+      api_key: this._apikey,
+      action: method
+    });
+
+    if (SmsActivate.DEBUG) console.log('SmsActivate Request:', req);
+
+    const res: string = await requestp({
+      uri: this._url,
+      qs: req
+    });
+
+    if (SmsActivate.DEBUG) console.log('SmsActivate Response:', res);
+
+    return res;
+  }
+}
+
+module.exports = SmsActivate;
